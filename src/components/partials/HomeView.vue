ea4bffb91a641ea36659e1d1cc457a9de6a45f42
Md. Shahinur Islam <shahin_amen@yahoo.com>
2025-03-16 21:10:43 +0600

    color

--- src/components/partials/HomeView.vue
+++ src/components/partials/HomeView.vue
@@ -1,7 +1,9 @@
 <template>
   <section class="bg-slate-100 min-h-screen flex flex-col justify-center items-center">
-    <h1 class="text-3xl md:text-5xl font-bold text-center text-slate-900">
-      Welcome to the Home View
+    <h1
+      class="text-3xl md:text-5xl font-bold text-center bg-gradient-to-r from-fuchsia-700 to-orange-600 bg-clip-text text-transparent"
+    >
+      Let's jump in Vue JS and Tailwind CSS!
     </h1>
     <p class="text-center md:text-2xl text-slate-700">This is a partial component</p>
   </section>
