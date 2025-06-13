a1852c7114c000555e560d99947c07cebff0f6cb
Md. Shahinur Islam <shahin_amen@yahoo.com>
2025-06-13 12:30:00 +0600

    bug fixed

--- src/components/partials/CardView.vue
+++ src/components/partials/CardView.vue
@@ -13,15 +13,15 @@ export default {
         :title="'Unique Visitors'"
         :value="24.7"
         :percentage="20"
-        :icon="'fa fa-solid fa-check-circle'"
+        :icon="'fa fa-solid fa-check-circle'" class="cursor-pointer"
       />
-      <Card :title="'Total Pageviews'" :value="29.5" :percentage="40" :icon="'fa-solid fa-star'" />
-      <Card :title="'Bounce Rate'" :value="48.8" :percentage="60" :icon="'fa-solid fa-thumbs-up'" />
+      <Card :title="'Total Pageviews'" :value="29.5" :percentage="40" :icon="'fa-solid fa-star'" class="cursor-pointer" />
+      <Card :title="'Bounce Rate'" :value="48.8" :percentage="60" :icon="'fa-solid fa-thumbs-up'" class="cursor-pointer" />
       <Card
         :title="'Visit Duration'"
         :value="21.4"
         :percentage="-50"
-        :icon="'fa-solid fa-face-smile'"
+        :icon="'fa-solid fa-face-smile'" class="cursor-pointer"
       />
     </div>
   </section>
